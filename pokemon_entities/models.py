da39cdbc815ecc819d4bfd00be34c78f03c3f0b9
Evgeniy Alehnovich <alehnovich90@yandex.ru>
2022-02-14 22:09:30 +0300

    add evolution to pokemon page

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -6,7 +6,9 @@ class Pokemon(models.Model):
     title_jp = models.CharField(max_length=200, blank=True)
     image = models.ImageField(upload_to='')
     description = models.TextField(blank=True)
-    previous_evolution = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+    previous_evolution = models.ForeignKey(
+        'self', on_delete=models.SET_NULL, null=True, blank=True, related_name='next_evolutions'
+    )
 
     def __str__(self):
         return self.title
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -64,16 +64,29 @@ def show_pokemon(request, pokemon_id):
         'img_url': pokemon.image.url,
     }
 
-    if pokemon.previous_evolution:
-        start_pokemon = pokemon.previous_evolution
-        pokemon_parameters.update({
-            'previous_evolution': {
-                "title_ru": start_pokemon.title,
-                "pokemon_id": start_pokemon.id,
-                "img_url": start_pokemon.image.url,
+    if not pokemon.next_evolutions.all():
+        pokemon_parameters.update(
+            {
+                'previous_evolution': {
+                    'title_ru': pokemon.previous_evolution.title,
+                    'pokemon_id': pokemon.previous_evolution.id,
+                    'img_url': pokemon.previous_evolution.image.url,
+                }
             }
-        })
+        )
 
+    if not pokemon.previous_evolution:
+        start_pokemon = pokemon.next_evolutions.all().first()
+        pokemon_parameters.update(
+            {
+                'next_evolution': {
+                    'title_ru': start_pokemon.title,
+                    'pokemon_id': start_pokemon.id,
+                    'img_url': start_pokemon.image.url,
+                }
+            }
+        )
+    print(pokemon_parameters)
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon_entity in PokemonEntity.objects.filter(pokemon=pokemon):
